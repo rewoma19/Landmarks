86eaa1662091ba3c3f121afb515cef82e401f3f3
Raphael <rewoma19@gmail.com>
2025-10-29 22:35:52 -0600

    Create and combine starter views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Raphael Odeareduo on 2025-10-29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
